fb1576a4e7a383fd9e79d2f1bc4ae1bd6627170d
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2024-09-10 01:17:38 +0800

    feat: add desktop shortcut for c001apk

--- pkgs/uncategorized-DataEraserC/c001apk-flutter/c001apk-flutter.nix
+++ pkgs/uncategorized-DataEraserC/c001apk-flutter/c001apk-flutter.nix
@@ -3,9 +3,10 @@
   sources,
   pkg-config,
   flutter,
+  makeDesktopItem,
+  copyDesktopItems,
 }:
 flutter.buildFlutterApplication rec {
-
   inherit (sources.c001apk-flutter) pname version src;
 
   sourceRoot = "${src.name}";
@@ -13,8 +14,34 @@ flutter.buildFlutterApplication rec {
 
   nativeBuildInputs = [
     pkg-config
+    copyDesktopItems
   ];
 
+  postInstall = ''
+    _postinstall() {
+      for n in 16 32 48 64 128 256 1024; do
+        size=$n"x"$n
+        install -Dm644 ${src}/assets/icon/icon.png $out/share/icons/hicolor/$size/apps/${pname}.png
+      done
+    }
+    _postinstall
+  '';
+  desktopItems = lib.toList (makeDesktopItem {
+    name = pname;
+    genericName = pname;
+    exec = pname;
+    icon = pname;
+    comment = pname;
+    desktopName = pname;
+    categories = [ "Network" ];
+    extraConfig = {
+      "Name[en_US]" = pname;
+      "Name[zh_CN]" = "酷市场";
+      "Name[zh_TW]" = "酷市场";
+      "Comment[zh_CN]" = "酷市场";
+      "Comment[zh_TW]" = "酷市场";
+    };
+  });
   meta = with lib; {
     description = "c001apk";
     homepage = "https://github.com/bggRGjQaUbCoE/c001apk-flutter";
