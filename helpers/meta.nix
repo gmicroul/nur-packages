ab11d4ff707d9357e9292c546144a5f37431e0ce
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2024-09-27 16:56:53 +0800

    fix cachix public key

--- helpers/meta.nix
+++ helpers/meta.nix
@@ -3,7 +3,7 @@ rec {
   atticUrl = "https://attic.xuyh0120.win/lantian";
   atticPublicKey = "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc=";
   cachixUrl = "https://dataeraserc.cachix.org";
-  cachixPublicKey = "dataeraserc.cachix.org-1:VB/XTSZLKIg0sLnZxr+op45pII4pInaOP8pbfgUJ+Uc=";
+  cachixPublicKey = "dataeraserc.cachix.org-1:t0lYPod3nkn0ijiOzjwT57MmBDeJnxVurvV8ZdPpIHo=";
   garnixUrl = "https://cache.garnix.io";
   garnixPublicKey = "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g=";
 
